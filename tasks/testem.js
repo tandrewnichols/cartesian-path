aa459bf973cea13a53bb9de288675feb77ea6df3
Andrew Nichols <tandrewnichols@gmail.com>
2016-12-28 23:20:51 -0500

    Phantom on travis is sub 2.0, so I still need bind-polyfill for testem tests.

--- tasks/testem.js
+++ tasks/testem.js
@@ -4,6 +4,7 @@ module.exports = {
       'node_modules/should/should.js',
       'dist/expand-path.js',
       'node_modules/mocha-given/browser/mocha-given.js',
+      'test/helpers/bind-polyfilljs',
       'test/helpers/setup.js',
       'test/*.coffee'
     ],
